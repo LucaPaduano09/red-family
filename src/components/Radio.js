cf58628f8807033986fc04f27340ad9bf63de20e
Luca Paduano <l.paduano@reply.it>
2022-05-04 15:43:28 +0200

    puntata 6

--- src/components/Radio.js
+++ src/components/Radio.js
@@ -15,7 +15,7 @@ const Radio = () => {
             <Zoom>
               <video width="80%" controls>
                 <source
-                  src="./images/puntata-5.mp4#t=0.001"
+                  src="./images/puntata-6.mp4#t=0.001"
                   preload="metadata"
                   type="video/mp4"
                 />
@@ -67,6 +67,17 @@ const Radio = () => {
               <video width="80%" controls>
                 <source
                   src="./images/puntata-4.mp4#t=0.001"
+                  preload="metadata"
+                  type="video/mp4"
+                />
+              </video>
+            </Zoom>
+          </ImageWrapper2>
+          <ImageWrapper2>
+            <Zoom>
+              <video width="80%" controls>
+                <source
+                  src="./images/puntata-5.mp4#t=0.001"
                   preload="metadata"
                   type="video/mp4"
                 />
