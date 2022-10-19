b30483542d3302ac193511087b584ca8f4a4b057
Luca Paduano <lucapaduano09@outlook.it>
2022-10-19 20:59:25 +0200

    si

--- src/components/home/HomeFinal.jsx
+++ src/components/home/HomeFinal.jsx
@@ -35,28 +35,29 @@ const HomeFinal = () => {
       <Gallery
         videos={[
           {
+            src: "/images/puntata-2-radio2.mp4",
+            title: "red radio2 pt 2",
+            type: "project",
+            id: "Reset",
+            size: "story"
+          },
+          {
             src: "/images/puntata-1-radio2.mp4",
             title: "red radio2 pt 1",
             type: "project",
-            id: "Reset",
+            id: "1",
             size: "story"
           },
           {
             src: "/images/puntata-11.mp4",
             title: "red radio pt 11",
             type: "project",
-            id: "1",
+            id: "2",
           },
           {
             src: "https://www.youtube.com/embed/01QbeS4G8FM",
             title: "red family tranquill",
             type: "youtube",
-            id: "2",
-          },
-          {
-            src: "/images/puntata-10.mp4",
-            title: "red radio pt 10",
-            type: "project",
             id: "3",
           },
         ]}
